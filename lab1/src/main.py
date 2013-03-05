b28049db5e328efd52f20d24df1e2c3143273c5c
aamihailov <alexander.a.mihailov@gmail.com>
2013-03-05 02:56:13 +0100

    111

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -2,6 +2,8 @@
 
 from numpy import matrix
 import numpy as np
+from numpy import linalg as la
+from scipy.optimize import minimize
 
 
 class Plan(object):
@@ -10,32 +12,54 @@ class Plan(object):
         q = int(0.5 * s * (s + 1) + 1)
         self.q = q                                          # Число точек
         self.A = np.matrix(np.ndarray((self.s, self.q)))    # Матрица плана
-        self.p = np.array([1.0 / q] * q)                    # Веса точек
+        self.p = np.array(np.ndarray((self.q, )))
+        self.p[:] = 1.0 / q                              # Веса точек
 
-    def inf_matrix(self):
-        A = self.A
-        p = self.p
+    def inf_matrix(self, f, A=None, p=None):
+        if A is None:
+            A = self.A
+        if p is None:
+            p = self.p
         q = self.q
-        return sum(A[:, i] * A[:, i].transpose() * p[i] for i in xrange(0, q))
+        return sum(f(A[:, i]) * f(A[:, i]).transpose() * p[i] for i in xrange(q))
 
-    def grad_by_alpha(self):
-        return -np.log(np.linalg.det(self.inf_matrix()))
+    def grad_by_alpha(self, f, A=None, p=None):
+        return -np.log(la.det(self.inf_matrix(f, A, p)))
 
-    def grad_by_p(self, f):
-        return
+    def grad_by_p(self, f, A=None, p=None):
+        return -np.log(la.det(self.inf_matrix(f, A, p)))
+        # if A is None:
+        #     A = self.A
+        # q = self.q
+        # return np.array([-(la.inv(self.inf_matrix(f, A, p)) * f(A[:, i]) * f(A[:, i]).transpose()).trace()[0, 0]
+        #                  for i in xrange(q)])
+
+
+def build_plan_dirgrad(f, xi0):
+    """Синтез оптимального плана с помощью прямой градиентной процедуры. D критерий.
+    """
+    xi = xi0
+
+    exit_cond = 1.0e+10
+    detM = la.det(xi.inf_matrix(f))                 # Определитель информационной матрицы
+    while exit_cond > 1.0e-3:
+        bnds = ((0.0, 1.0), ) * xi.q                # Границы для весов p: [0.0 .. 1.0]
+        cons = ({'type': 'eq', 'fun': lambda x: sum(x) - 1},)   # Нормированность суммы весов
+        res = minimize(lambda p: xi.grad_by_p(f, p=p),
+                       x0=xi.p,
+                       method='SLSQP',
+                       bounds=bnds,
+                       constraints=cons)
+        pass
 
 
 def main():
     xi = Plan(2)
     xi.A[:, :] = [[1, -1,  1,  3],
                   [1,  2, -2,  0]]
-    f    = lambda alpha: np.matrix([[alpha[0]   ],
-                                    [alpha[1]**2]])
-    dMdA = [lambda alpha: np.matrix([[2*alpha[0],  alpha[1]**2],
-                                     [alpha[1]**2, 0          ]]),
-            lambda alpha: np.matrix([[0,                   2*alpha[0]*alpha[1]],
-                                     [2*alpha[0]*alpha[1], 4*alpha[1]**3]])]
-
+    f    = lambda alpha: np.matrix([[alpha[0, 0]   ],
+                                    [alpha[1 ,0]**2]])
+    build_plan_dirgrad(f, xi)
 
 
 
