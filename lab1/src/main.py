5607db921487060f1d10aaacc47cdcfcaaef9c0d
aamihailov <alexander.a.mihailov@gmail.com>
2013-03-06 13:13:29 +0100

    procedure for building informational map is done. Some changes in Plan class

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -4,9 +4,11 @@ import numpy as np
 from numpy import linalg as la
 from scipy.optimize import minimize
 
+import pylab as pl
+
 
 class Plan(object):
-    def __init__(self, s, q = None):
+    def __init__(self, s, q=None):
         self.s = s                                          # Число регрессоров
         if q is None:
             self.q = int(0.5 * s * (s + 1) + 1)                 # Число точек
@@ -15,6 +17,23 @@ class Plan(object):
         self.A = np.matrix(np.ndarray((self.s, self.q)))    # Матрица плана: [s,q]
         self.p = np.matrix(np.ndarray((self.q, 1)))
         self.p[:, :] = 1.0 / self.q                         # Веса точек: вектор, или матрица [q,1]
+
+    def set_bounds(self, bounds):
+        self.bnds = bounds
+
+    def _remove_ith_point(self, i):
+        self.p = np.delete(self.p, i, 0)
+        self.A = np.delete(self.A, i, 1)
+        self.q -= 1
+
+    def reduce_plan(self):
+        epsilon = 1.0e-5
+        i = 0
+        while i < self.q:
+            if self.p[i, 0] < epsilon:
+                self._remove_ith_point(i)
+            else:
+                i += 1
 
     def partial_inf_matrix(self, i, f, A=None):
         if A is None:
@@ -42,18 +61,29 @@ class Plan(object):
     def eta(self, f):
         return self.s
 
+    def __str__(self):
+        return '%s\n%s' % (self.A, self.p)
 
-def build_plan_dirgrad(f, xi, epsilon=1.0e-3):
+
+def build_plan_dirgrad(f, xi, epsilon=1.0e-6):
     """Синтез оптимального плана с помощью прямой градиентной процедуры. D критерий.
     """
     exit_cond = np.inf
     iter = 0
     while exit_cond > epsilon:
         iter += 1
-        detM = la.det(xi.inf_matrix(f))
+        detM = np.log(la.det(xi.inf_matrix(f)))
         mu   = xi.mu(f)
-        print 'On iter %d:\n%s\n%s\n\ndet(M(xi)) = %.3lf\nmu(alpha,xi) = %.3lf\n==================' % (
-            iter, xi.A, xi.p, detM, mu)
+        print 'On iter %d:\n%s\n\nlog(det(M(xi))) = %.3lf\nmu(alpha,xi) = %.3lf\n==================' % (
+            iter, xi, detM, mu)
+
+        bnds = reduce(lambda a, b: a + b, [(xi.bnds[i], ) * xi.q for i in xrange(xi.s)])     # Границы для точек плана
+        res = minimize(lambda A: xi.X(f, A=np.asmatrix(A).reshape((xi.s, xi.q))),
+                       x0=np.squeeze(np.asarray(xi.A.reshape((1, xi.s * xi.q)))),
+                       method='L-BFGS-B',
+                       bounds=bnds)
+        A_new = np.matrix(res['x']).reshape((xi.s, xi.q))
+        xi.A[:, :] = A_new
 
         bnds = ((0.0, 1.0), ) * xi.q                            # Границы для весов p: [0.0 .. 1.0]
         cons = ({'type': 'eq', 'fun': lambda p: sum(p) - 1},)   # Нормированность суммы весов
@@ -64,35 +94,77 @@ def build_plan_dirgrad(f, xi, epsilon=1.0e-3):
                        constraints=cons)
         p_new = np.matrix(res['x']).reshape((xi.q, 1))
 
-        bnds = ((-5.0, 5.0), ) * xi.s * xi.q                    # Границы для точек плана A: [-5.0 .. 5.0]
-        res = minimize(lambda A: xi.X(f, A=np.asmatrix(A).reshape((xi.s, xi.q))),
-                       x0=np.squeeze(np.asarray(xi.A)),
-                       method='SLSQP',
-                       bounds=bnds)
-        A_new = np.matrix(res['x']).reshape((xi.s, xi.q))
-
-        exit_cond = sum(np.linalg.norm(A_new[:,i] - xi.A[:,i])**2 + (p_new[i,0] - xi.p[i,0])**2 for i in xrange(xi.q))
-        xi.A[:,:] = A_new
-        xi.p[:,:] = p_new
-    detM = la.det(xi.inf_matrix(f))
+        exit_cond = sum(np.linalg.norm(A_new[:, i] - xi.A[:, i])**2 + (p_new[i, 0] - xi.p[i, 0])**2 for i in xrange(xi.q))
+        xi.p[:, :] = p_new
+    detM = np.log(la.det(xi.inf_matrix(f)))
     mu   = xi.mu(f)
-    print 'Finally:\n%s\n%s\n\ndet(M(xi)) = %.3lf\nmu(alpha,xi) = %.3lf\n==================' % (
-        xi.A, xi.p, detM, mu)
+    print 'Finally:\n%s\n\nlog(det(M(xi))) = %.3lf\nmu(alpha,xi) = %.3lf\n==================' % (
+        xi, detM, mu)
+
+    xi.reduce_plan()
+    detM = np.log(la.det(xi.inf_matrix(f)))
+    mu   = xi.mu(f)
+    print 'After reduction:\n%s\n\nlog(det(M(xi))) = %.3lf\nmu(alpha,xi) = %.3lf\n==================' % (
+        xi, detM, mu)
+    return xi
+
+
+def build_plan_dirscan(f, xi0):
+    base = np.log(la.det(xi0.inf_matrix(f)))
+    print 'Base plan: \n%s\nlog(det(M))=%.3lf\n=================\n' % (xi0, base)
+
+    xi = Plan(xi0.s, xi0.q + 1)
+    xi.set_bounds(xi0.bnds)
+    xi.A[:, :-1] = xi0.A[:, :]
+    xi.A[:,  -1] = 0
+    xi.p[:-1, :] = xi0.p * (float(xi0.q) / (xi0.q + 1))
+    xi.p[ -1, :] = 1.0 / (xi0.q + 1)
+
+    def variate_point(f, xi, i, x, y):
+        xi.A[0, i] = x
+        xi.A[1, i] = y
+        return la.det(xi.inf_matrix(f))
+
+    func = lambda x, y: variate_point(f, xi, -1, x, y) - base
+    dx = (xi.bnds[0][1] - xi.bnds[1][0]) / 100
+    dy = (xi.bnds[1][1] - xi.bnds[1][0]) / 100
+    x = np.arange(xi.bnds[0][0], xi.bnds[0][1] + dx, dx)
+    y = np.arange(xi.bnds[1][0], xi.bnds[1][1] + dy, dy)
+    X, Y = pl.meshgrid(x, y)
+    Z = np.array([[func(xv, yv) for xv in x] for yv in y])
+    pl.pcolor(X, Y, Z)
+    pl.xlim(xi.bnds[0])
+    pl.ylim(xi.bnds[1])
+    pl.colorbar()
+    pl.title('Improving plan from base det(M) = %.3lf by adding a point' % base)
+
+    ij = np.unravel_index(Z.argmax(), Z.shape)
+    xi.A[0, -1] = X.item(ij)
+    xi.A[1, -1] = Y.item(ij)
+    print 'Found plan: \n%s\nlog(det(M))=%.3lf\n=================\n' % (xi, np.log(la.det(xi.inf_matrix(f))))
+
+    pl.show()
+
     return xi
 
 
 def main():
-    s  = 2
-    q  = 5
-    f  = lambda alpha: np.matrix([[np.sin(alpha[0, 0])],
-                                  [np.cos(alpha[1, 0])]])
+    s = 2
+    q = 2
+    f = lambda alpha: np.matrix([[np.cos(alpha[0,0])],
+                                 [np.sin(alpha[1,0])]])
+    x0 = -5; x1 = 5
+    A = (x1 - x0) * np.random.random((s, q)) + x0      # starting with random plan
+    # A = [[-5, 5], [-5, 5]]
     xi = Plan(s, q)
-    xi.A[:, :] = [[-5,  5, 5, -5, 1],
-                  [ 5, -5, 5, -5, 0]]
-    xi = build_plan_dirgrad(f, xi)
-
+    xi.A[:, :] = A
+    xi.set_bounds(((-5.0, 5.0), (-5.0, 5.0)))
+    build_plan_dirgrad(f, xi)
     print 'Checking solution for being optimal (less is better): [%.2lf]' % np.abs(xi.mu(f) - xi.eta(f))
+
+    build_plan_dirscan(f, xi)
 
 
 if __name__ == '__main__':
+    np.set_printoptions(precision=3)
     main()
